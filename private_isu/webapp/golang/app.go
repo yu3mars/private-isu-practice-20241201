ec616ff742a3bc4c3c2025e10c9266f8e3214871
yu3mars <yu3mars@users.noreply.github.com>
2024-12-07 19:43:27 +0900

    feat: pprofを追加

--- private_isu/webapp/golang/app.go
+++ private_isu/webapp/golang/app.go
@@ -16,6 +16,8 @@ import (
 	"strings"
 	"time"
 
+	_ "net/http/pprof"
+
 	"github.com/bradfitz/gomemcache/memcache"
 	gsm "github.com/bradleypeabody/gorilla-sessions-memcache"
 	"github.com/go-chi/chi/v5"
@@ -792,6 +794,10 @@ func postAdminBanned(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	go func() {
+		fmt.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	host := os.Getenv("ISUCONP_DB_HOST")
 	if host == "" {
 		host = "localhost"
